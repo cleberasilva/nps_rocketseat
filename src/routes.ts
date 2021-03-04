c2c0f089da216ec346294b56e84990f2b74f1b7c
cleberasilva <cleberspark@gmail.com>
2021-03-03 22:13:20 -0300

    sending emails

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { UsersController } from './controllers/UsersController';
 import { SurveysController } from './controllers/SurveysController';
+import { SendMailController } from './controllers/SendMailController';
 
 const router = Router();
 
+const sendMailController = new SendMailController()
 const usersController = new UsersController();
 const surveysController = new SurveysController();
 
@@ -11,5 +13,7 @@ router.post("/users",usersController.create);
 
 router.get("/surveys", surveysController.show);
 router.post("/surveys", surveysController.create);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
