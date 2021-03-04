a7819a9e02ec517e2ef1c0e156d4db817b64aafa
cleberasilva <cleberspark@gmail.com>
2021-03-04 20:18:36 -0300

    adding validations

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import { UsersController } from './controllers/UsersController';
 import { SurveysController } from './controllers/SurveysController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswersController } from './controllers/AnswersController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
-const sendMailController = new SendMailController()
+const sendMailController = new SendMailController();
 const usersController = new UsersController();
 const surveysController = new SurveysController();
+const answersController = new AnswersController();
+const npsController = new NpsController();
 
 router.post("/users",usersController.create);
 
@@ -15,5 +19,9 @@ router.get("/surveys", surveysController.show);
 router.post("/surveys", surveysController.create);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answersController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
 
 export { router };
